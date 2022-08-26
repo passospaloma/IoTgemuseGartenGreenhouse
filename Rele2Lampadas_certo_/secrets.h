36e9347e176a205d2c615223fd2f879aeaf5fea3
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-25 17:29:15 -0700

    Update secrets.h

--- Rele2Lampadas_certo_/secrets.h
+++ Rele2Lampadas_certo_/secrets.h
@@ -1,7 +1,7 @@
-#define SECRET_SSID "Westeros"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "7kingdoms" // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS "" // replace MyPassword with your WiFi password
 
 
-#define SECRET_CH_ID 638073     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "4G2S5SZUQPQZR1DB"   // replace XYZ with your channel write API Key
-#define SECRET_AUTH "92af498048fd46e1aa72d1543cd4c099"
+#define SECRET_CH_ID      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY ""   // replace XYZ with your channel write API Key
+#define SECRET_AUTH ""
