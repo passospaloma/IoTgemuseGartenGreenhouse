c1c1f361ce2610efc5e1e27199e4b4550548a835
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 17:58:14 -0700

    Delete releteste directory

--- releteste/secrets.h
+++
@@ -1,9 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "Baranha1"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "pa081281" // replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 638073     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "4G2S5SZUQPQZR1DB"   // replace XYZ with your channel write API Key
-#define SECRET_AUTH "92af498048fd46e1aa72d1543cd4c099"
