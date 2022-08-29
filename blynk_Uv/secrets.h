25ff1d76a91b5067a1065280a8e7548107efe3f4
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 18:12:02 -0700

    Update secrets.h

--- blynk_Uv/secrets.h
+++ blynk_Uv/secrets.h
@@ -1,7 +1,7 @@
 // Secrets.h to save all credentials e secret connections
 
 
-#define SECRET_SSID "Baranha"    // SSID WiFi network name
-#define SECRET_PASS "pa081281" // WiFi password
+#define SECRET_SSID ""    // SSID WiFi network name
+#define SECRET_PASS "" // WiFi password
 
-#define SECRET_AUTH " 1d39e80e4c2d43829c5aed8d050da422" // Blynk Auth
+#define SECRET_AUTH " " // Blynk Auth
