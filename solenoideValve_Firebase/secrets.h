0928d9718270a89ad109ff5166871d6bd16aa6ac
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 17:57:27 -0700

    Update secrets.h

--- solenoideValve_Firebase/secrets.h
+++ solenoideValve_Firebase/secrets.h
@@ -1,10 +1,10 @@
 // Use this file to store all of the private credentials
 // and connection details
 
-#define SECRET_SSID "Baranha"    // MySSID WiFi network name
-#define SECRET_PASS "pa081281" // WiFi password
+#define SECRET_SSID ""    // MySSID WiFi network name
+#define SECRET_PASS "" // WiFi password
 
-#define SECRET_AUTH " 1d39e80e4c2d43829c5aed8d050da422"  // blynk auth
+#define SECRET_AUTH " "  // blynk auth
 
-#define SECRET_HOST  "imetagrotech19.firebaseio.com" //Firebase host
-#define SECRET_AUTHF "uvpTlERWQ9JfIlwmxqL5WG2P98WTyoePV1PuRX1t" //Firebase auth
+#define SECRET_HOST  "" //Firebase host
+#define SECRET_AUTHF "" //Firebase auth
