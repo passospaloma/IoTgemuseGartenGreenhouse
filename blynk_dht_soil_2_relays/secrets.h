6e4d6630a6429355b6a3d2d76d8d24c56b0224cd
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 18:09:49 -0700

    Update secrets.h

--- blynk_dht_soil_2_relays/secrets.h
+++ blynk_dht_soil_2_relays/secrets.h
@@ -1,7 +1,7 @@
 // Use this file to store all of the private credentials
 // and connection details
 
-#define SECRET_SSID "Baranha"    // MySSID WiFi network name
-#define SECRET_PASS "pa081281" // WiFi password
+#define SECRET_SSID ""    // MySSID WiFi network name
+#define SECRET_PASS "" // WiFi password
 
-#define SECRET_AUTH " 1d39e80e4c2d43829c5aed8d050da422"  // blynk auth
+#define SECRET_AUTH " "  // blynk auth
