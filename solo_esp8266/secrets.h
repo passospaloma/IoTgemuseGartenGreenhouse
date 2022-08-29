8223df1cf41cbbc5643f2f32ce0d134fc8f9d220
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 17:56:15 -0700

    Update secrets.h

--- solo_esp8266/secrets.h
+++ solo_esp8266/secrets.h
@@ -1,12 +1,12 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "CAIPIRINHA"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "pa081281" // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS "" // replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 638073     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "4G2S5SZUQPQZR1DB"   // replace XYZ with your channel write API Key
-#define SECRET_AUTH "92af498048fd46e1aa72d1543cd4c099"
+#define SECRET_CH_ID      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY ""   // replace XYZ with your channel write API Key
+#define SECRET_AUTH ""
 
-#define SECRET_HOST  "imetagrotech19.firebaseio.com"
-#define SECRET_AUTHF "uvpTlERWQ9JfIlwmxqL5WG2P98WTyoePV1PuRX1t"
+#define SECRET_HOST  ""
+#define SECRET_AUTHF ""
