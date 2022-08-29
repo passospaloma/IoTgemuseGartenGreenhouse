17486fa9104f105e4f2f92752d291326b88a007d
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-08-28 18:08:26 -0700

    Update secrets.h

--- blynk_segundo_dht_soil/secrets.h
+++ blynk_segundo_dht_soil/secrets.h
@@ -1,7 +1,7 @@
 // Secrets.h serve para guardar todos as credenciais e conexões secretas
 
 
-#define SECRET_SSID "Baranha"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "pa081281" // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS "" // replace MyPassword with your WiFi password
 
-#define SECRET_AUTH " 1d39e80e4c2d43829c5aed8d050da422"
+#define SECRET_AUTH " "
