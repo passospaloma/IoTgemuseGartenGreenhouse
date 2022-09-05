2e7cbf859a3f8affce61bcdfc03ea4a0c061ebf6
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-09-04 18:50:47 -0700

    Delete blynk_dht directory

--- blynk_dht/secrets.h
+++
@@ -1,7 +0,0 @@
-// Secrets.h serve para guardar todos as credenciais e conexões secretas
-
-
-#define SECRET_SSID ""    // replace MySSID with your WiFi network name
-#define SECRET_PASS "" // replace MyPassword with your WiFi password
-
-#define SECRET_AUTH ""
