14f0534e2d370a57440e7db86c4f0a4eaaf9d9e6
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-09-04 18:51:12 -0700

    Delete bLYNK_SOIL_DHT_MODERN directory

--- bLYNK_SOIL_DHT_MODERN/secrets.h
+++
@@ -1,7 +0,0 @@
-// Secrets.h serve para guardar todos as credenciais e conexões secretas
-
-
-#define SECRET_SSID "Baranha"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "pa081281" // replace MyPassword with your WiFi password
-
-#define SECRET_AUTH " 1d39e80e4c2d43829c5aed8d050da422"
